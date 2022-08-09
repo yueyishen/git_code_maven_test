5430866bf0003ee77f15645df86f6dd11c0dfc07
yueysh <1183328605@qq.com>
2022-08-09 12:54:44 +0800

    2

--- src/main/java/com/yueysh/git/GitTest.java
+++ src/main/java/com/yueysh/git/GitTest.java
@@ -2,7 +2,7 @@ package com.yueysh.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git2222222222");
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git4");
